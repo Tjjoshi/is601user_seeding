73c8e1fc5c3bcef1308e0f1842057cbabb8cc02e
toral <tj244@njit.edu>
2018-10-24 13:22:27 -0400

    Task: created 50 seeds in UserTableSeeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,11 +11,8 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => str_random(10),
-            'email' => str_random(10).'@gmail.com',
-            'password' => bcrypt('secret'),
-        ]);
-
+        factory(App\User::class, 50)->create()->each(function ($u) {
+           // $u->posts()->save(factory(App\Post::class)->make());
+        });
     }
 }
